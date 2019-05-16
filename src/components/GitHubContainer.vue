cd08ef6bdb97a0cab9d39b10baba9acd26991a84
Oleg_Valerievich <vitiukoleh@gmail.com>
2019-05-16 18:53:51 +0300

    oath test

--- src/components/GitHubContainer.vue
+++ src/components/GitHubContainer.vue
@@ -8,18 +8,39 @@ import { clientId, clientSecret } from "../config";
 export default {
   name: "GitHubContainer",
   mounted: function() {
-    const params = new URLSearchParams(window.location.search);
-    if (params.has("code")) {
-      localStorage.setItem("accessToken", params.get("code"));
-    }
+    this.setAccessToken();
+  },
+  methods: {
+    setAccessToken: async () => {
+      const params = new URLSearchParams(window.location.search);
 
-    const accessToken = localStorage.getItem("accessToken");
-    console.log(accessToken);
+      if (params.has("code")) {
+        const code = params.get("code");
 
-    if (!accessToken) {
-      window.location.replace(
-        `https://github.com/login/oauth/authorize?client_id=${clientId}`
-      );
+        try {
+          const res = await axios.post(
+            "https://github.com/login/oauth/access_token",
+            {
+              client_id: clientId,
+              client_secret: clientSecret,
+              code
+            }
+          );
+          console.log(res);
+          localStorage.setItem("accessToken", res.data.access_token);
+        } catch (e) {
+          console.err(e.message);
+        }
+      } else {
+        const accessToken = localStorage.getItem("accessToken");
+        console.log(accessToken);
+
+        if (!accessToken) {
+          window.location.replace(
+            `https://github.com/login/oauth/authorize?client_id=${clientId}`
+          );
+        }
+      }
     }
   }
 };
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,10 @@
+import axios from 'axios';
+
 const clientId = '0d6007f1caa2a8a928a5';
 const clientSecret = '93cb979cc2ec6b3a439002295051cd1ce33ce9c6';
 
-export { clientId, clientSecret };
+const api = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+export { clientId, clientSecret, api };
